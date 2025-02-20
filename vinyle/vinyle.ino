6f17a93f655c76ccd9e61d2d8b5a9b7971793606
Anais5s <anaisboisson@gmail.com>
2025-02-20 08:48:55 +0100

    modif potentiometres

---
+++ vinyle/vinyle.ino
@@ -0,0 +1,27 @@
+#include <Audio.h>
+#include "vinyle.h"
+
+vinyle inyle;
+
+AudioInputI2S audioInput;
+AudioOutputI2S out;
+AudioControlSGTL5000 audioShield;
+AudioConnection patchCord0(audioInput, 0, inyle, 0); // Envoie le signal gauche à Faust
+AudioConnection patchCord1(audioInput, 1, inyle, 1); // Envoie le signal droit à Faust
+AudioConnection patchCord2(inyle, 0, out, 0);        // Sortie gauche
+AudioConnection patchCord3(inyle, 1, out, 1);        // Sortie droite
+void setup() {
+  AudioMemory(12);  
+  audioShield.enable();
+  audioShield.adcHighPassFilterDisable(); //enleve le filtre passe-haut present par defaut
+  audioShield.lineInLevel(1); //reduit le gain d'entree
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  int sensorValue2 = analogRead(A1);
+  inyle.setParamValue("effet vinyle",0.1+(0.01*sensorValue));
+  inyle.setParamValue("saturation",0.01+(0.01*sensorValue));
+  delay(50);
+  audioShield.volume(0.001*sensorValue2);
+}
