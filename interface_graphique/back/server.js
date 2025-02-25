ff46a2de4694e9d4ae0d17f3e652c55fb1c51c4d
cb <charlesbouquet1011@gmail.com>
2025-02-25 14:52:42 +0100

    poster et style

--- interface_graphique/back/server.js
+++ interface_graphique/back/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
